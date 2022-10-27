76225b453d6093898e3ae0022919e652d02b7760
Lara Bucher <bucher.lara@web.de>
2022-10-27 21:28:28 +0200

    Next routing

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,29 @@
 import styled from "styled-components";
+import Link from "next/link";
+import StyledNavLink from "../components/StyledNavLink";
+import { useRouter } from "next/router";
 
 export default function Header() {
+  const { pathname } = useRouter();
   return (
     <StyledHeader>
       <StyledNav>
-        <a>Home</a>
-        <a>Product</a>
-        <a>Team</a>
-        <a>Account</a>
+        <Link href="/" legacyBehavior passHref>
+          <StyledNavLink active={pathname === "/"}>Home</StyledNavLink>
+        </Link>
+        <Link href="/product" legacyBehavior passHref>
+          <StyledNavLink active={pathname === "/product"}>
+            Product
+          </StyledNavLink>
+        </Link>
+        <Link href="/team" legacyBehavior passHref>
+          <StyledNavLink active={pathname === "/team"}>Team</StyledNavLink>
+        </Link>
+        <Link href="/account" legacyBehavior passHref>
+          <StyledNavLink active={pathname === "/account"}>
+            Account
+          </StyledNavLink>
+        </Link>
       </StyledNav>
     </StyledHeader>
   );
